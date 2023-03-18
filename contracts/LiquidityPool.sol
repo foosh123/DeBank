2886734fe4384f43d0171f91ac3149cf6c9424b0
KaushikR1999 <kaushik05@hotmail.co.uk>
2023-03-18 16:12:38 +0800

    Updated LiquidityPool Logic

--- contracts/LiquidityPool.sol
+++ contracts/LiquidityPool.sol
@@ -1,86 +1,158 @@
 pragma solidity >= 0.5.0;
-
-// import './ERC20.sol';
+// pragma experimental ABIEncoderV2;
 
 contract LiquidityPool {
 
-    enum currency { BTC, ETH, BNB, USDT, SOL, XRP }
+    // { BTC, ETH, BNB, USDT, SOL, XRP }
 
-    uint256 poolAmount;
-    uint256 borrowerInterestRate;
-    uint256 lenderInterestRate;
+    // address[] borrowersList;
+    // address[] lendersList;
+    address public owner;
+    string[] currencyTypes;
 
-    address[] borrowersList;
-    address[] lendersList;
+    // mapping(string => uint256) poolAmounts;
+    // mapping(string => uint256) borrowerInterestRates;
+    // mapping(string => uint256) lenderInterestRates;
     
-    mapping(address => uint256) public borrowers;
-    mapping(address => uint256) public lenders;
+    // mapping(address => uint256) public borrowers;
+    // mapping(address => uint256) public lenders;
 
-    constructor() public {
-        poolAmount = 0;
-        borrowerInterestRate = 0;
-        lenderInterestRate = 0;
+    // mapping(address => string) borrowerCurrencyType;
+    // mapping(address => string) lenderCurrencyType;
+
+    // constructor() public {
+    //     poolAmount = 0;
+    //     borrowerInterestRate = 0;
+    //     lenderInterestRate = 0;
+    // }
+
+    struct liquidityPool {
+        string currencyType;
+        uint256 poolAmount;       
+        uint256 borrowerInterestRate;
+        uint256 lenderInterestRate;
+        address owner;
+        address prevOwner;
+        mapping(address => uint256) borrowers;
+        mapping(address => uint256) lenders;        
     }
 
-    event DepositMade(address lender, currency choiceOfCurrency, uint256 depositAmount);
-    event WithdrawalMade(address borrower, currency choiceOfCurrency, uint256 withdrawnAmount);
+    uint256 public numPools = 0;
+    mapping(string => liquidityPool) public pools;
 
-    function checkBalance() public view returns (uint256) {
-        return poolAmount;
+    event DepositMade(address lender, string choiceOfCurrency, uint256 depositAmount);
+    event WithdrawalMade(address borrower, string choiceOfCurrency, uint256 withdrawnAmount);
+    event LogOwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    //modifier to ensure a function is callable only by its owner    
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
     }
 
-    function depositToLiquidityPool(currency choiceOfCurrency, uint256 depositAmount) public {
+    function Owned() public {
+        owner = msg.sender;
+    }
 
-        require (depositAmount > 0, "Can't deposit 0 tokens");
+    function transferOwnership(address newOwner) public ownerOnly {
+        require(newOwner != address(0));
+        emit LogOwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
 
-        uint256 existingAmount = lenders[msg.sender];
-        if (existingAmount != 0) {
-            existingAmount += depositAmount;
-        } else {
-            existingAmount = depositAmount;
+    // Only owner can create pools
+    function addNewPool(
+        string memory currencyType,
+        uint256 borrowerInterestRate, // change to API
+        uint256 lenderInterestRate // change to API
+    ) public payable ownerOnly returns(uint256) {
+        // require(numberOfSides > 0);
+        //require((msg.value) > 0.00 ether, "Please deposit a minimum amount to initiate a new currency pool");
+        
+        //new pool object
+        liquidityPool memory newLiquidityPool = liquidityPool(
+            currencyType,
+            msg.value,     
+            borrowerInterestRate,
+            lenderInterestRate,
+            msg.sender,  //owner
+            address(0) //prev owner
+        );
+        
+        // uint256 newPoolId = numPools++;
+        pools[currencyType] = newLiquidityPool; //commit to state variable
+        currencyTypes.push(currencyType);
+        return numPools;   //return new poolId
+    }
+
+
+    function checkPoolAmount (string memory choiceOfCurrency) public view returns (uint256) {
+        return pools[choiceOfCurrency].poolAmount;
+    }
+
+    // function getListOfCurrencyTypes() public view returns (string memory) {
+    //     string memory result = "";
+    //     for (uint i=0; i<currencyTypes.length; i++) {
+    //         string.concat(result, currencyTypes[i]);
+    //     }        
+    //     return result;
+    // }
+
+    function depositToLiquidityPool(string memory choiceOfCurrency, uint256 depositAmount) public {
+
+        liquidityPool storage pool = pools[choiceOfCurrency];
+
+        bool isValidCurrency = false;
+        for (uint i=0; i<currencyTypes.length; i++) {
+            if(keccak256(abi.encodePacked(currencyTypes[i]))==keccak256(abi.encodePacked(choiceOfCurrency))) {
+                isValidCurrency = true;
+            }
         }
+        require (isValidCurrency == true, "The Currency is not supported yet!");
+        require (depositAmount > 0, "Can't deposit 0 tokens");
+        
+        // uint256 existingAmount = pool.lenders[msg.sender];
+        pool.lenders[msg.sender] += depositAmount;
 
-        poolAmount += depositAmount;
-        lenders[msg.sender] = existingAmount;
-        lendersList.push(msg.sender);
+        pool.poolAmount += depositAmount;
+        // pool.lenders[msg.sender] = existingAmount;
+        // pool.lendersList.push(msg.sender);
 
         emit DepositMade(msg.sender, choiceOfCurrency, depositAmount);
     }
 
-    function withdrawFromLiquidityPool(currency choiceOfCurrency, uint256 withdrawalAmount) public {
+    function withdrawFromLiquidityPool(string memory choiceOfCurrency, uint256 withdrawalAmount) public {
 
-        require (lenders[msg.sender] >= withdrawalAmount, "Not enough tokens to withdraw");
+        liquidityPool storage pool = pools[choiceOfCurrency];
+        bool isValidCurrency = false;
+        for (uint i=0; i<currencyTypes.length; i++) {
+            if(keccak256(abi.encodePacked(currencyTypes[i]))==keccak256(abi.encodePacked(choiceOfCurrency))) {
+                isValidCurrency = true;
+            }
+        }
+        require (isValidCurrency == true, "The Currency is not supported yet!");
+        require (pool.lenders[msg.sender] >= withdrawalAmount, "Not enough tokens to withdraw");
 
-        uint256 existingAmount = lenders[msg.sender];
-        existingAmount -= withdrawalAmount;
-        poolAmount -= withdrawalAmount;
-        lenders[msg.sender] = existingAmount;
+        pool.lenders[msg.sender] = pool.lenders[msg.sender] - withdrawalAmount;
+        pool.poolAmount -= withdrawalAmount;
 
         emit WithdrawalMade(msg.sender, choiceOfCurrency, withdrawalAmount);
     }
 
-    function getNumberOfBorrowers() public view returns (uint256) {
-        return borrowersList.length;
+    function getBorrowerLoanAmount(string memory choiceOfCurrency, address borrower) public view returns (uint256) {
+        return pools[choiceOfCurrency].borrowers[borrower];
     }
 
-    function getNumberOfLenders() public view returns (uint256) {
-        return lendersList.length;
+    function getLenderLoanAmount(string memory choiceOfCurrency, address lender) public view returns (uint256) {
+        return pools[choiceOfCurrency].lenders[lender];
     }
 
-    function getBorrowerLoanAmount(address borrower) public view returns (uint256) {
-        return borrowers[borrower];
+    function getBorrowerInterestRate(string memory choiceOfCurrency) public view returns (uint256) {
+        return pools[choiceOfCurrency].borrowerInterestRate;
     }
 
-    function getLenderLoanAmount(address lender) public view returns (uint256) {
-        return lenders[lender];
-    }
-
-    function getBorrowerInterestRate() public view returns (uint256) {
-        return borrowerInterestRate;
-    }
-
-    function getLenderInterestRate() public view returns (uint256) {
-        return lenderInterestRate;
+    function getLenderInterestRate(string memory choiceOfCurrency) public view returns (uint256) {
+        return pools[choiceOfCurrency].lenderInterestRate;
     }
 
 
