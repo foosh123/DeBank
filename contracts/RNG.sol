68660710ac31ff19afa96c9b7127f0b283ff145a
Qingyi1115 <xqy1115@gmail.com>
2023-04-02 16:43:05 +0800

    Added Random Number Generator

---
+++ contracts/RNG.sol
@@ -0,0 +1,14 @@
+pragma solidity >= 0.5.0;
+
+
+contract RNG{
+    uint8 fakeRandomNumber;
+
+    function generateRandonNumber() public view returns (uint8){
+        return fakeRandomNumber;
+    }
+
+    function setRandomNumber(uint8 forcedNumber) public{
+         fakeRandomNumber = forcedNumber;
+    }
+}
