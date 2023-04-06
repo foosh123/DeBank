558915ae574a38a02757983e8de50d7aaaa93d80
tiara2711 <77134793+tiara2711@users.noreply.github.com>
2023-04-07 01:10:49 +0800

    Add files via upload

--- contracts/Helper.sol
+++ contracts/Helper.sol
@@ -2,7 +2,14 @@ pragma solidity >= 0.5.0;
 
 import "./ERC20.sol";
 import "./DSMath.sol";
-import "./PriceConsumerBTCUSD.sol";
+import "./PriceConsumer.sol";
+import "./RNG.sol";
+import "./Cro.sol";
+import "./Shib.sol";
+import "./Uni.sol";
+// import "./User.sol";
+//import all tokens 1,2,3
+
 // import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract Helper {
@@ -11,14 +18,19 @@ contract Helper {
     //     uint256 platformFee;
     //     uint256 transactionFee; 
     // }
-    PriceConsumerBTCUSD btc_usd;
+
+    ERC20 erc20Contract;
+    RNG r = new RNG();
+    Cro cro = new Cro();
+    Shib shib = new Shib();
+    Uni uni = new Uni();
     string public currentConversion;
     uint256 public platformFee;
     uint256 public transactionFee;
-     constructor() public {
-        // platformFee = platFee;
-        // transactionFee = transFee;
-    }
+    //  constructor() public {
+    //     // platformFee = platFee;
+    //     // transactionFee = transFee;
+    // }
 
     function setCurrencyConversion(string memory conversion) public { // {'btc_usd', etc}
         currentConversion = conversion;
@@ -32,36 +44,44 @@ contract Helper {
         transactionFee = amt;
     }
 
-    // function getPlatformFee() public view returns (uint256) {
-    //     return platformFee;
-    // }
-
-    // function getTransactionFee() public view returns (uint256) {
-    //     return transactionFee;
-    // }
-
-    //assuming that the function that calls for charging platform/transaction fee is called by user
-    function chargePlatformFee() public payable {
-        //require that user has more money than platform fee
-        require(msg.sender.balance >= platformFee);
-        address payable recipient = address(uint160(address(this)));
-        recipient.transfer(transactionFee); //transferring transaction and platform fees to helper contract?
+    //before using, check if address to charge has more than the platform fee
+    function chargePlatformFee(uint256 choiceOfCurrency, address user, address recipient) public payable {
+        if(choiceOfCurrency == 0) { //means its token1
+            // require(user.balance >= platformFee);
+            cro.sendToken(user, recipient, platformFee);
+        } else if(choiceOfCurrency == 1) {
+            // require(user.balance >= platformFee);
+            shib.sendToken(user, recipient, platformFee);  
+        } else if(choiceOfCurrency == 2) {
+            // require(user.balance >= platformFee);
+            uni.sendToken(user, recipient, platformFee); 
+        }
     }
 
-    function chargeTransactionFee() public payable {
-        //require that user has more money than transaction fee
-        require(msg.sender.balance >= transactionFee);
-        //do we need to check that userID's owner is msg.sender?
-        address payable recipient = address(uint160(address(this)));
-        recipient.transfer(transactionFee);
+    //before using, check if address to charge has more than the transaction fee
+    function chargeTransactionFee(int tokenType, address user, address recipient) public payable {
+        if(choiceOfCurrency == 0) { //means its token1
+            // require(user.balance >= platformFee);
+            cro.sendToken(user, recipient, transactionFee);
+        } else if(choiceOfCurrency == 1) {
+            // require(user.balance >= platformFee);
+            shib.sendToken(user, recipient, transactionFee);  
+        } else if(choiceOfCurrency == 2) {
+            // require(user.balance >= platformFee);
+            uni.sendToken(user, recipient, transactionFee); 
+        }
+    }        
 
-    }
-
-    function withdrawFee(string memory currency, uint256 amt) public {
-        //might need api for this
-        // uint256 convertedAmt = 
-        //check if transferring to the correct person
-        // msg.sender.transfer(convertedAmt);
+    function withdrawFee(uint256 choiceOfCurrency, uint256 amt, address from) public {
+        if(choiceOfCurrency == 0) { //choiceOfCurrency == 0 
+            require(cro.checkBalance() >= amt, "You dont have enough token to deposit");
+            cro.sendToken(from, msg.sender, amt);
+        } else if(choiceOfCurrency == 1) {
+            require(shib.checkBalance() >= amt, "You dont have enough token to deposit");
+            shib.sendToken(from, msg.sender, amt);
+        } else if(choiceOfCurrency == 2) {
+            require(uni.checkBalance() >= amt, "You dont have enough token to deposit");
+            uni.sendToken(from, msg.sender, amt);
     }
 
 }
