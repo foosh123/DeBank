9fead52681298288b2b91ad50dc38afe2de742ca
tiara2711 <77134793+tiara2711@users.noreply.github.com>
2023-04-07 18:36:34 +0800

    fixed transaction and platform fees

--- contracts/Helper.sol
+++ contracts/Helper.sol
@@ -3,7 +3,7 @@ pragma solidity >= 0.5.0;
 import "./ERC20.sol";
 import "./DSMath.sol";
 import "./PriceConsumer.sol";
-import "./RNG.sol";
+// import "./RNG.sol";
 import "./Cro.sol";
 import "./Shib.sol";
 import "./Uni.sol";
@@ -12,69 +12,79 @@ import "./Uni.sol";
 
 contract Helper {
 
-    ERC20 erc20Contract;
-    RNG r = new RNG();
+    // ERC20 erc20Contract;
+    // RNG r = new RNG();
     Cro cro = new Cro();
     Shib shib = new Shib();
     Uni uni = new Uni();
-    string public currentConversion;
-    uint256 public platformFee;
-    uint256 public transactionFee;
+    uint256 public platformFee; //in ether
+    uint256 public transactionFee; //in ether
     //  constructor() public {
     //     // platformFee = platFee;
     //     // transactionFee = transFee;
     // }
 
-    function setCurrencyConversion(string memory conversion) public { // {'btc_usd', etc}
-        currentConversion = conversion;
-    }
+    // function setCurrencyConversion(string memory conversion) public {
+    //     currentConversion = conversion;
+    // }
 
-    function setPlatformFee(uint256 amt) public {
-        platformFee = amt;
-    }
+    // function setPlatformFee(uint256 amt) public {
+    //     platformFee = amt;
+    // }
 
-    function setTransactionFee(uint256 amt) public {
-        transactionFee = amt;
-    }
+    // function setTransactionFee(uint256 amt) public {
+    //     transactionFee = amt;
+    // }
 
     //before using, check if address to charge has more than the platform fee
     function chargePlatformFee(uint256 choiceOfCurrency, address user, address recipient) public payable {
-        if(choiceOfCurrency == 0) { //means its token1
-            require(cro.checkBalance(user) >= platformFee, "Insufficient tokens");
-            cro.sendToken(user, recipient, platformFee);
+        platformFee = 100;
+        if(choiceOfCurrency == 0) { 
+            uint256 convertedFee = platformFee/100;
+            require(cro.checkBalance(user) >= convertedFee, "Insufficient tokens");
+            cro.sendToken(user, recipient, convertedFee);
         } else if(choiceOfCurrency == 1) {
-            require(shib.checkBalance(user) >= platformFee, "Insufficient tokens");
-            shib.sendToken(user, recipient, platformFee);  
+            uint256 convertedFee = platformFee/10000;
+            require(shib.checkBalance(user) >= convertedFee, "Insufficient tokens");
+            shib.sendToken(user, recipient, convertedFee);  
         } else if(choiceOfCurrency == 2) {
-            require(uni.checkBalance(user) >= platformFee, "Insufficient tokens");
-            uni.sendToken(user, recipient, platformFee); 
+            uint256 convertedFee = platformFee/10;
+            require(uni.checkBalance(user) >= convertedFee, "Insufficient tokens");
+            uni.sendToken(user, recipient, convertedFee); 
         }
     }
 
     //before using, check if address to charge has more than the transaction fee
-    function chargeTransactionFee(int tokenType, address user, address recipient) public payable {
-        if(choiceOfCurrency == 0) { //means its token1
-            require(cro.checkBalance(user) >= transactionFee, "Insufficient tokens");
-            cro.sendToken(user, recipient, transactionFee);
+    function chargeTransactionFee(uint256 choiceOfCurrency, address user, address recipient) public payable {
+        transactionFee = 100;
+        if(choiceOfCurrency == 0) { 
+            uint256 convertedFee = transactionFee/100;
+            require(cro.checkBalance(user) >= convertedFee, "Insufficient tokens");
+            cro.sendToken(user, recipient, convertedFee);
         } else if(choiceOfCurrency == 1) {
-            require(shib.checkBalance(user) >= transactionFee, "Insufficient tokens");
-            shib.sendToken(user, recipient, transactionFee);  
+            uint256 convertedFee = transactionFee/10000;
+            require(shib.checkBalance(user) >= convertedFee, "Insufficient tokens");
+            shib.sendToken(user, recipient, convertedFee);  
         } else if(choiceOfCurrency == 2) {
-            require(uni.checkBalance(user) >= transactionFee, "Insufficient tokens");
-            uni.sendToken(user, recipient, transactionFee); 
-        }
-    }        
+            uint256 convertedFee = transactionFee/10;
+            require(uni.checkBalance(user) >= convertedFee, "Insufficient tokens");
+            uni.sendToken(user, recipient, convertedFee); 
+            }
+    }  
 
     function withdrawFee(uint256 choiceOfCurrency, uint256 amt, address from) public {
-        if(choiceOfCurrency == 0) { //choiceOfCurrency == 0 
-            require(cro.checkBalance(from) >= amt, "Insufficient tokens");
-            cro.sendToken(from, msg.sender, amt);
+        if(choiceOfCurrency == 0) { 
+            uint256 convertedAmt = amt/100;
+            require(cro.checkBalance(from) >= convertedAmt, "Insufficient tokens");
+            cro.sendToken(from, msg.sender, convertedAmt);
         } else if(choiceOfCurrency == 1) {
-            require(shib.checkBalance(from) >= amt, "Insufficient tokens");
-            shib.sendToken(from, msg.sender, amt);
+            uint256 convertedAmt = amt/10000;
+            require(shib.checkBalance(from) >= convertedAmt, "Insufficient tokens");
+            shib.sendToken(from, msg.sender, convertedAmt);
         } else if(choiceOfCurrency == 2) {
-            require(uni.checkBalance(from) >= amt, "Insufficient tokens");
-            uni.sendToken(from, msg.sender, amt);
-    }
-
+            uint256 convertedAmt = amt/10;
+            require(uni.checkBalance(from) >= convertedAmt, "Insufficient tokens");
+            uni.sendToken(from, msg.sender, convertedAmt);
+        }
+    }   
 }
